c03cd01b0c01e02866fd785b5440dcf5d0dd2739
Pradyumna Singh <pradyumna.209301309@muj.manipal.edu>
2024-02-22 04:05:28 +0530

    user model and auth routes jwt

---
+++ server/DbConnect/DbConnect.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+async function dbConnect() {
+  mongoose
+    .connect(
+        process.env.MONGODB_URI
+    )
+    .then(() => {
+      console.log("Successfully connected to MongoDB Atlas!");
+    })
+    .catch((error) => {
+      console.log("Unable to connect to MongoDB Atlas!");
+      console.error(error);
+    });
+}
+
+module.exports = dbConnect;
--- server/server.js
+++ server/server.js
@@ -1,60 +1,147 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const dbConnect = require('./DbConnect/DbConnect');
+const User = require('./Schema/UserSchema')
+const auth = require("./auth");
+
+dotenv.config(); 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
+dbConnect();
+
+//cors handling
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+
 
 // MongoDB connection URI
-const uri = "mongodb+srv://pradydevs:1kqHoUq5vu7DCUDb@custord.3lnuwjb.mongodb.net/custord";
 
-// Connect to MongoDB using Mongoose
-mongoose.connect(uri);
-
-// Get the default connection
-const db = mongoose.connection;
-
-// Check for MongoDB connection errors
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// Once connected, log a success message
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-// Create a user schema
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-});
-
-// Create a User model based on the user schema
-const User = mongoose.model('User', userSchema);
 
 // Use body-parser middleware to parse POST request data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Route to insert a new user
-app.post('/register', async (req, res) => {
-  try {
-    const { username, password } = req.body;
+// register endpoint
+app.post("/register", (request, response) => {
+  // hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      // create a new user instance and collect the data
+      const user = new User({
+        name: request.body.name,
+        email: request.body.email,
+        password: hashedPassword,
+      });
 
-    // Create a new user instance
-    const newUser = new User({ username, password });
-
-    // Save the new user to the database
-    await newUser.save();
-
-    res.send('User added successfully');
-  } catch (error) {
-    console.error('Error adding user:', error);
-    res.status(500).send('Internal Server Error');
-  }
+      // save the new user
+      user
+        .save()
+        // return success if the new user is added to the database successfully
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        // catch error if the new user wasn't added successfully to the database
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// login endpoint
+app.post("/login", (request, response) => {
+  // check if email exists
+  User.findOne({ email: request.body.email })
+
+    // if email exists
+    .then((user) => {
+      // compare the password entered and the hashed password found
+      bcrypt
+        .compare(request.body.password, user.password)
+
+        // if the passwords match
+        .then((passwordCheck) => {
+
+          // check if password matches
+          if(!passwordCheck) {
+            return response.status(400).send({
+              message: "Passwords does not match",
+              error,
+            });
+          }
+
+          //   create JWT token
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+
+          //   return success response
+          response.status(200).send({
+            message: "Login Successful",
+            email: user.email,
+            token,
+          });
+        })
+        // catch error if password does not match
+        .catch((error) => {
+          response.status(400).send({
+            message: "Passwords does not match",
+            error,
+          });
+        });
+    })
+    // catch error if email does not exist
+    .catch((e) => {
+      response.status(404).send({
+        message: "Email not found",
+        e,
+      });
+    });
 });
+
+app.get("/free-endpoint", (request, response) => {
+  response.json({ message: "You are free to access me anytime" });
+});
+
+// authentication endpoint
+app.get("/auth-endpoint", auth, (request, response) => {
+  response.json({ message: "You are authorized to access me" });
+});
+
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
